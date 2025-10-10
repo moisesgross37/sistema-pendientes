6f737c086e08d4755361de3db77cb9d655262038
moisesgross37 <moisesgross37@gmail.com>
2025-10-10 15:42:30 -0400

    Update pendientes.controller.ts

--- backend/src/pendientes/pendientes.controller.ts
+++ backend/src/pendientes/pendientes.controller.ts
@@ -8,30 +8,30 @@ import {
   UseGuards,
   UseInterceptors,
   UploadedFiles,
-  Res,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
 import { extname } from 'path';
-import type { Response } from 'express';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { PendientesService } from './pendientes.service';
 import { CreatePendienteDto } from './dto/create-pendiente.dto';
 import { UpdatePendienteDto } from './dto/update-pendiente.dto';
 
 @Controller('pendientes')
-// @UseGuards(JwtAuthGuard) // <-- BORRAMOS EL GUARDIA DE LA PUERTA PRINCIPAL
 export class PendientesController {
   constructor(private readonly pendientesService: PendientesService) {}
 
   @Post('upload')
-  @UseGuards(JwtAuthGuard) // Ponemos un guardia aquí
+  @UseGuards(JwtAuthGuard)
   @UseInterceptors(
     FilesInterceptor('files', 5, {
       storage: diskStorage({
-        destination: './uploads',
+        destination: '/opt/render/project/src/uploads', // Usamos la ruta del disco persistente
         filename: (req, file, cb) => {
-          const randomName = Array(32).fill(null).map(() => Math.round(Math.random() * 16).toString(16)).join('');
+          const randomName = Array(32)
+            .fill(null)
+            .map(() => Math.round(Math.random() * 16).toString(16))
+            .join('');
           return cb(null, `${randomName}${extname(file.originalname)}`);
         },
       }),
@@ -46,25 +46,25 @@ export class PendientesController {
   }
 
   @Post()
-  @UseGuards(JwtAuthGuard) // Ponemos un guardia aquí
+  @UseGuards(JwtAuthGuard)
   create(@Body() createPendienteDto: CreatePendienteDto) {
     return this.pendientesService.create(createPendienteDto);
   }
 
   @Get()
-  @UseGuards(JwtAuthGuard) // Ponemos un guardia aquí
+  @UseGuards(JwtAuthGuard)
   findAll() {
     return this.pendientesService.findAll();
   }
 
   @Get(':id')
-  @UseGuards(JwtAuthGuard) // Ponemos un guardia aquí
+  @UseGuards(JwtAuthGuard)
   findOne(@Param('id') id: string) {
     return this.pendientesService.findOne(+id);
   }
 
   @Patch(':id')
-  @UseGuards(JwtAuthGuard) // Ponemos un guardia aquí
+  @UseGuards(JwtAuthGuard)
   update(
     @Param('id') id: string,
     @Body() updatePendienteDto: UpdatePendienteDto,
