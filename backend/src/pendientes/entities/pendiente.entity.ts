397784521ade5317fbe59bfa94c86acffcd5b213
moisesgross37 <moisesgross37@gmail.com>
2025-10-28 20:12:14 -0400

    Update pendiente.entity.ts

--- backend/src/pendientes/entities/pendiente.entity.ts
+++ backend/src/pendientes/entities/pendiente.entity.ts
@@ -1,49 +1,49 @@
 import { Usuario } from '../../usuarios/entities/usuario.entity';
 import {
-  Column,
-  CreateDateColumn,
-  Entity,
-  ManyToOne,
-  PrimaryGeneratedColumn,
-  UpdateDateColumn, // Importación nueva
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn, 
 } from 'typeorm';
 
 @Entity({ name: 'pendientes' })
 export class Pendiente {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @PrimaryGeneratedColumn()
+  id: number;
 
-  @CreateDateColumn()
-  fechaCreacion: Date;
+  @CreateDateColumn()
+  fechaCreacion: Date;
 
-  @Column({ type: 'timestamp', nullable: true }) // <-- NUEVA COLUMNA
-  fechaAsignacion: Date | null;
+  @Column({ type: 'timestamp', nullable: true }) 
+  fechaAsignacion: Date | null;
 
-  @Column({ type: 'timestamp', nullable: true }) // <-- NUEVA COLUMNA
-  fechaConclusion: Date | null;
+  @Column({ type: 'timestamp', nullable: true }) 
+  fechaConclusion: Date | null;
 
-  @Column()
-  nombreCentro: string;
+  @Column()
+  nombreCentro: string;
 
-  @Column()
-  descripcion: string;
+  @Column()
+  descripcion: string;
 
-  @Column({
-    type: 'enum',
-    enum: ['Por Asignar', 'Iniciado', 'Fuera de oficina', 'Concluido', 'En administración'],
-    default: 'Por Asignar',
-  })
-  status: string;
-  
-  @Column({ default: false })
-  archivado: boolean;
+  @Column({
+    type: 'enum',
+    enum: ['Por Asignar', 'Iniciado', 'Fuera de oficina', 'Concluido', 'En administración'],
+    default: 'Por Asignar',
+  })
+  status: string;
+  
+  @Column({ default: false })
+  archivado: boolean;
 
-  @Column({ type: 'simple-array', nullable: true })
-  imagenes?: string[];
+  @Column({ type: 'simple-array', nullable: true })
+  imagenes?: string[];
 
-  @ManyToOne(() => Usuario, { eager: true })
-  asesor: Usuario;
+  @ManyToOne(() => Usuario, { eager: true })
+  asesor: Usuario;
 
-  @ManyToOne(() => Usuario, { nullable: true, eager: true })
-  colaboradorAsignado: Usuario;
+  @ManyToOne(() => Usuario, { nullable: true, eager: true })
+  colaboradorAsignado: Usuario | null; // <--- AQUÍ ESTÁ LA CORRECCIÓN
 }
