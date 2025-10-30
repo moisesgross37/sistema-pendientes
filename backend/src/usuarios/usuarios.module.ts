a28040c20345df674156f52e2d14559ac4c12c71
moisesgross37 <moisesgross37@gmail.com>
2025-10-30 08:53:29 -0400

    Update usuarios.module.ts

--- backend/src/usuarios/usuarios.module.ts
+++ backend/src/usuarios/usuarios.module.ts
@@ -1,13 +1,17 @@
+// backend/src/usuarios/usuarios.module.ts
+
 import { Module } from '@nestjs/common';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Usuario } from './entities/usuario.entity';
+import { Pendiente } from '../pendientes/entities/pendiente.entity'; // <--- 1. IMPORTAR PENDIENTE
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuario])],
-  controllers: [UsuariosController],
-  providers: [UsuariosService],
-  exports: [UsuariosService], // <-- Se añade aquí, al final
+  controllers: [UsuariosController],
+  providers: [UsuariosService],
+  // 👇 2. AÑADIR 'Pendiente' AL forFeature
+  imports: [TypeOrmModule.forFeature([Usuario, Pendiente])],
+  exports: [UsuariosService],
 })
 export class UsuariosModule {}
